bdc9339151426f2125d08070e41e3e5f9e5d0cb8
Sony <62589156+sony7139@users.noreply.github.com>
2020-07-23 22:11:58 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,14 @@
+#creating a tuple
+tuple1 = (2, 4, 6, 8, 10,'python', 'tuple')
+
+l = len(tuple1)
+print("The length of the tuple1 is: ")
+print(l)
+      
+#accessing the elements from a tuple
+print("The elements of the list are: ")
+for i in tuple1:
+    print(i)  
+    
+if tuple1[0]:
+    print("the tuple1 elements are being accessed")
